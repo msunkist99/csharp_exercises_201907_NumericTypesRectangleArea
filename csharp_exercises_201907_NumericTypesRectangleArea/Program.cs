aa783d761eba81682b5f471b85a51c5953c9169c
msunkist99 <msunkist99@gmail.com>
2019-12-06 15:13:04 -0600

    Add project files.

---
+++ csharp_exercises_201907_NumericTypesRectangleArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace csharp_exercises_201907_NumericTypesRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int length;
+            //int width;
+            //int area;
+
+            Console.WriteLine("enter rectangle length - ");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter rectangle width - ");
+            int width = int.Parse(Console.ReadLine());
+            int area = length * width;
+            Console.WriteLine("Area of the rectangle is " + area);
+
+            //Console.WriteLine("Area of the rectangle is " + (length * width));
+
+            Console.ReadLine();
+        }
+    }
+}
